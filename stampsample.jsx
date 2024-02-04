0d7147fd5ae18acd0c459fdf501da7b445f2b75b
iicafiaxus <iica@live.jp>
2024-02-04 14:35:16 +0900

    implove sample text

--- stampsample.jsx
+++ stampsample.jsx
@@ -1,7 +1,10 @@
 const StampSample = function(props){
 	return <div className={`sample ${props.theme}`}>
 		<p>
-			これは本当に{props.stamp}です
+			これは本当に{props.stamp}ですね。よろしくお願いします{
+			}<span className="stamp-group">{props.stamp}{props.stamp}</span>
+			<br />
+			よろしくお願いします。
 			<br />
 			<span className="stamp selected">{props.stamp} 1</span>
 			<span className="stamp">{props.stamp} 1</span>
