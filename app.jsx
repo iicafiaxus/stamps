7d0f535a7b594cf9b1694f122411670c65b39587
iicafiaxus <iica@live.jp>
2024-02-10 02:38:00 +0900

    improve config panel design

--- app.jsx
+++ app.jsx
@@ -36,42 +36,54 @@ const App = function(props){
 			<div className="source">
 				<h3>組版する文字列</h3>
 				<p><textarea onChange={handleChangeText} ref={textareaRef} /></p>
-				<h3>文字の色</h3>
-				<p><ColorSelector name="color" setColor={setColor} default="#0a0" /></p>
-				<h3>フォント</h3>
-				<p><Selector name="font-family" setValue={setFontFamily} default="BIZ UDPMincho" options={[
-					{ value: "BIZ UDPMincho", title: "明朝" },
-					{ value: "BIZ UDPGothic", title: "ゴシック" }
-				]} /></p>
-				<h3>背景</h3>
-				<p><Selector name="transparency" setValue={setTransparency} default="0" options={[
-					{ value: "0", title: "透明" },
-					{ value: "8", title: "半透明" },
-					{ value: "f", title: "不透明" }
-				]} /></p>
-				{transparency != "0" && <p>
-					<ColorSelector name="backgroundColor" setColor={setBackgroundColor} default="#000" />
-				</p>}
-				<h3>余白</h3>
-				<p><Selector name="padding-style" setValue={setPaddingStyle} default="normal" options={[
-					{ value: "none", title: "なし" },
-					{ value: "normal", title: "四角" },
-					{ value: "round", title: "角丸" }
-				]} /></p>
-				<h3>改行調整</h3>
-				<p><Selector name="compression-limit" setValue={setCompressionLimit} default={2.0} options={[
-					{ value: 1.0, title: "しない" },
-					{ value: 1.25, title: "弱" },
-					{ value: 2.0, title: "中" },
-					{ value: 3.0, title: "強" },
-					{ value: 10.0, title: "最強" },
-				]} /></p>
-				<h3>詰め方</h3>
-				<p><Selector name="spacing-policy" setValue={setSpacingPolicy} default="normal" options={[
-					{ value: "normal", title: "普通" },
-					{ value: "proportional", title: "比率固定" },
-					{ value: "hard", title: "激詰め(未実装)", enabled: false },
-				]} /></p>
+				<div className="config-item">
+					<h3>文字の色</h3>
+					<p><ColorSelector name="color" setColor={setColor} default="#0a0" /></p>
+				</div>
+				<div className="config-item">
+					<h3>フォント</h3>
+					<p><Selector name="font-family" setValue={setFontFamily} default="BIZ UDPMincho" options={[
+						{ value: "BIZ UDPMincho", title: "明朝" },
+						{ value: "BIZ UDPGothic", title: "ゴシック" }
+					]} /></p>
+				</div>
+				<div className="config-item">
+					<h3>背景</h3>
+					<p><Selector name="transparency" setValue={setTransparency} default="0" options={[
+						{ value: "0", title: "透明" },
+						{ value: "8", title: "半透明" },
+						{ value: "f", title: "不透明" }
+					]} /></p>
+					{transparency != "0" && <p>
+						<ColorSelector name="backgroundColor" setColor={setBackgroundColor} default="#000" />
+					</p>}
+				</div>
+				<div className="config-item">
+					<h3>余白</h3>
+					<p><Selector name="padding-style" setValue={setPaddingStyle} default="normal" options={[
+						{ value: "none", title: "なし" },
+						{ value: "normal", title: "四角" },
+						{ value: "round", title: "角丸" }
+					]} /></p>
+				</div>
+				<div className="config-item">
+					<h3>改行調整</h3>
+					<p><Selector name="compression-limit" setValue={setCompressionLimit} default={2.0} options={[
+						{ value: 1.0, title: "しない" },
+						{ value: 1.25, title: "弱" },
+						{ value: 2.0, title: "中" },
+						{ value: 3.0, title: "強" },
+						{ value: 10.0, title: "最強" },
+					]} /></p>
+				</div>
+				<div className="config-item">
+					<h3>詰め方</h3>
+					<p><Selector name="spacing-policy" setValue={setSpacingPolicy} default="normal" options={[
+						{ value: "normal", title: "普通" },
+						{ value: "proportional", title: "比率固定" },
+						{ value: "hard", title: "激詰め(未実装)", enabled: false },
+					]} /></p>
+				</div>
 			</div>
 			<div className="result">
 				<h3>結果</h3>
